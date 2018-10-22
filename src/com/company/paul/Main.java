6cbe4124bfbdd91b93c7b67e4a7f66c639fb8772
paulwrodriguez <paul.rodriguez@ucdenver.edu>
2018-10-21 18:26:37 -0600

    init commit

---
+++ src/com/company/paul/Main.java
@@ -0,0 +1,9 @@
+package com.company.paul;
+
+public class Main {
+
+    public static void main(String[] args) {
+        UrGame urGame = new UrGame();
+        urGame.play();
+    }
+}
---
+++ src/com/company/paul/UrGame.java
@@ -0,0 +1,8 @@
+package com.company.paul;
+
+public class UrGame {
+    
+    public void play() {
+        // print the board
+    }
+}
